dfc95089574608ff4462b3aa5b04057a2711e10b
Ben-Arlanda <ben.arlanda@protonmail.com>
2026-03-14 12:26:11 +1100

    scaffold initial serverless app foundation

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,78 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import {
+  HttpApi,
+  CorsHttpMethod,
+  HttpMethod,
+} from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const inspectionsBucket = new s3.Bucket(this, "InspectionsBucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const inspectionsTable = new dynamodb.Table(this, "InspectionsTable", {
+      partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const createInspectionFn = new NodejsFunction(this, "CreateInspectionFn", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(
+        __dirname,
+        "../../backend/src/handlers/createInspection.ts",
+      ),
+      handler: "handler",
+      environment: {
+        INSPECTIONS_TABLE_NAME: inspectionsTable.tableName,
+        INSPECTIONS_BUCKET_NAME: inspectionsBucket.bucketName,
+      },
+    });
+
+    inspectionsTable.grantReadWriteData(createInspectionFn);
+    inspectionsBucket.grantReadWrite(createInspectionFn);
+
+    const api = new HttpApi(this, "CarInspectionApi", {
+      corsPreflight: {
+        allowHeaders: ["content-type"],
+        allowMethods: [CorsHttpMethod.ANY],
+        allowOrigins: ["*"],
+      },
+    });
+
+    api.addRoutes({
+      path: "/inspections",
+      methods: [HttpMethod.POST],
+      integration: new HttpLambdaIntegration(
+        "CreateInspectionIntegration",
+        createInspectionFn,
+      ),
+    });
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.apiEndpoint,
+    });
+
+    new cdk.CfnOutput(this, "BucketName", {
+      value: inspectionsBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, "TableName", {
+      value: inspectionsTable.tableName,
+    });
   }
 }
